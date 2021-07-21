d2e9b094ddc145b9147974ab8e00e9b05c7dbfa5
Haitani Toshiki <haitani.toshiki@gmail.com>
2021-07-21 14:34:06 +0900

    first commit

---
+++ src/views/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 content-center">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="signUp">
+        Sign Up
+      </button>
+    </div>
+  </form>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Register',
+  setup: () => {
+  },
+  methods: {
+    signUp() {
+      console.log('test');
+      
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SignIn.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 content-center">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+    </div>
+    <div class="flex items-center justify-between mb-10">
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="login">
+        Sign in
+      </button>
+    </div>
+    <div class="flex items-center justify-between align-middle">
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline" @click="login">
+        <img class="float-left" src="../assets/btn_google_light_normal_ios.svg" /><div class="inline-block align-middle py-3">Sign in with Google</div>
+      </button>
+    </div>
+  </form>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'SignIn',
+  setup: () => {
+  },
+  methods: {
+    login() {
+      console.log('test');
+    },
+    singnInWithGoogle() {
+      console.log('test2'); 
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
